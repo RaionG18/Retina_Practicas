263d573939fbcb72fab3e0130c73bb1355d8777d
EmilioG-18062 <75913899+EmilioG-18062@users.noreply.github.com>
2021-10-08 05:49:12 -0600

    Update Arduino_MultiThreads_V2.ino

--- Arduino_MultiThreads_V2/Arduino_MultiThreads_V2.ino
+++ Arduino_MultiThreads_V2/Arduino_MultiThreads_V2.ino
@@ -16,27 +16,18 @@ DHT dht1(DHT1, DHTTYPE), dht2(DHT2, DHTTYPE), dht3(DHT3, DHTTYPE), dht4(DHT4, DH
 PZEM004T pzem(&Serial1);
 
 int puertaIn = 51, puertaOut = 50, cambios = 0;
+float Bus[1] = {};
 
 float Bus_Voltage[6] = {0, 0, 0, 0, 0, 0};
 float Bus_Current[6] = {0, 0, 0, 0, 0, 0};
 float Bus_Energy[6] = {0, 0, 0, 0, 0, 0};
 
+int tPZEM = 0, nPZEM = 0;
+
+int tBank = 0, nBank = 0, working = 0, Bateria = 0, first = 1;
+int Modulos[] = {38, 39, 40, 41};
 float Bus_Banco[4] = {0, 0, 0, 0};
 
-int tPZEM = 0;
-int nPZEM = 0;
-
-int tBank = 0;
-int nBank = 0;
-int working = 0;
-int Bateria = 0;
-int first = 1;
-int Modulos[] = {38, 39, 40, 41};
-
-float Bus[1] = {};
-//======================================================
-
-//====================== Pointers ======================
 byte ipPZEM[6][4] = {{192, 168, 1, 1}, {192, 168, 1, 2}, {192, 168, 1, 3}, {192, 168, 1, 4}, {192, 168, 1, 5}, {192, 168, 1, 6}};
 //======================================================
 
@@ -48,9 +39,8 @@ void setup() {
   pinMode (puertaOut, OUTPUT);
   for(int i; i <4; i++){
     pinMode(Modulos[i], OUTPUT);
-    digitalWrite(Modulos[i], HIGH);
+    digitalWrite(Modulos[i], LOW);
   }
-
 }
 
 void loop() {
@@ -68,6 +58,7 @@ void loop() {
   }
   //Lectura de Bancos de Baterias cada 0.250s
   if((millis()-tBank)>250){
+    Serial.print(nBank);
     if(working == 0){
       working = Change_Bank(nBank, first);
       tBank = millis();
@@ -98,7 +89,7 @@ void loop() {
   Serial.print("/");
   Serial.print("\r");
   cambios = 0;
-  //============
+  //=======================
   Combustible(); 
 }
 //======================================================
@@ -208,7 +199,36 @@ int Change_Bank(int i, int first){
       digitalWrite(bat, LOW);
     }
   }
-
+  
+  if(i == 0){
+    for(int a; a<4; a++){
+      pinMode(Modulos[a], OUTPUT);
+      digitalWrite(Modulos[a], LOW);
+    }
+    digitalWrite(Modulos[0], HIGH);
+  }
+  else if(i == 4){
+    for(int a; a<4; a++){
+      pinMode(Modulos[a], OUTPUT);
+      digitalWrite(Modulos[a], LOW);
+    }
+    digitalWrite(Modulos[1], HIGH);
+  }
+  else if(i == 8){
+    for(int a; a<4; a++){
+      pinMode(Modulos[a], OUTPUT);
+      digitalWrite(Modulos[a], LOW);
+    }
+    digitalWrite(Modulos[2], HIGH);
+  }
+  else if(i == 12){
+    for(int a; a<4; a++){
+      pinMode(Modulos[a], OUTPUT);
+      digitalWrite(Modulos[a], LOW);
+    }
+    digitalWrite(Modulos[3], HIGH);
+  }
+  
   if(digitalRead(bat) == LOW){
     digitalWrite(bat, HIGH);
   }
