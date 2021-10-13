3ea434490f3daffe42a242241728a38e0c801f59
EmilioG-18062 <75913899+EmilioG-18062@users.noreply.github.com>
2021-10-12 19:58:24 -0600

    Creación de JSON2SFTP

--- Data2JSON.py
+++ Data2JSON.py
@@ -4,7 +4,7 @@ import json
 packet = (1,2,3,4,5)
 
 named_tuple = time.localtime() # get struct_time
-time_string = time.strftime("%m_%d_%Y#%H-%M-%S", named_tuple)
+time_string = time.strftime("%m_%d_%Y#%H-%M-%S.json", named_tuple)
 
 with open('{}'.format(time_string), 'w') as outfile:
     json.dump(packet, outfile)
---
+++ JSON2SFTP.py
@@ -0,0 +1,24 @@
+import pysftp
+import glob
+import os.path
+
+def upload_file(file_path):
+
+    private_key = "~/.ssh/your-key.pem"  # can use password keyword in Connection instead
+    srv = pysftp.Connection(host="your-host", username="user-name", private_key=private_key)
+    srv.chdir('/var/web/public_files/media/uploads')  # change directory on remote server
+    srv.put(file_path)  # To download a file, replace put with get
+    srv.close()  # Close connection
+
+
+while True:
+    #Identificar el ultimo archivo creado
+    path = r'C:\Users\emili\OneDrive\Documents\GitHub\Retina_Practicas'
+    folder_path = r'{}'.format(path)
+    file_type = '\*json'
+    files = glob.glob(folder_path + file_type)
+    max_file = max(files, key=os.path.getctime)
+
+    #Enviar el archivo por SFTP
+    upload_file(max_file)
+
